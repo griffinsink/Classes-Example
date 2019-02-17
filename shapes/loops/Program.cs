a570271ec62ca234e7e8b0d96f79c38c8939921d
Griffin Sink <Griffin.D.Sink@ou.edu>
2019-02-17 13:13:43 -0600

    added loops2

--- shapes/loops/Program.cs
+++ shapes/loops/Program.cs
@@ -10,6 +10,22 @@ namespace loops
     {
         static void Main(string[] args)
         {
+            double score;
+            double totalScore = 0;
+            double average;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Please enter exam grade: ");
+                score = Convert.ToInt32(Console.ReadLine());
+                totalScore = totalScore + score;
+            }
+
+            average = totalScore / 3;
+
+            Console.WriteLine($"average exam score is: {average}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ shapes/loops2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double examScore;
+            double totalScore = 0;
+            double average;
+            string response;
+            int count = 0;
+
+
+            do
+            {
+                Console.WriteLine("Please enter your exam score: ");
+                examScore = Convert.ToInt32(Console.ReadLine());
+                totalScore = totalScore + examScore;
+                count++;
+
+                start:
+                Console.WriteLine("Do you have another score you wish to enter?");
+                response = Console.ReadLine().ToLower();
+
+                if (response == "yes")
+                {
+
+                }
+                else if (response == "no")
+                {
+                    Console.WriteLine("Thank you!");
+                }
+                else if (response != "yes" || response != "no")
+                {
+                    Console.WriteLine("Please enter yes or no");
+                    goto start;
+                }
+            } while (response == "yes" && response != "yes" || response != "no");
+
+            average = totalScore / count;
+
+            Console.WriteLine(average);
+
+
+            Console.ReadKey();
+
+               
+
+        }
+    }
+}
