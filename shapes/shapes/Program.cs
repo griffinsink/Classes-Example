40eec15a043c83d809ba60077693648b2ece117c
Griffin Sink <Griffin.D.Sink@ou.edu>
2019-02-17 12:37:36 -0600

    loop

--- shapes/shapes/Program.cs
+++ shapes/shapes/Program.cs
@@ -10,14 +10,27 @@ namespace shapes
     {
         static void Main(string[] args)
         {
+            List<Rectangle> rectangles = new List<Rectangle>();
+      
             Rectangle r1 = new Rectangle();
             r1.Length = 5;
             r1.Width = 5;
-            Console.WriteLine($"The perimeter for r1 is {r1.CalculatePerimeter()}, and the area is {r1.CalculateArea()}");
-            r1.CalculateArea();
+           // Console.WriteLine($"The perimeter for r1 is {r1.CalculatePerimeter()}, and the area is {r1.CalculateArea()}");
+
+            Rectangle r2 = new Rectangle()
+            {
+                Length = 10,
+                Width = 2
+            };
 
             Circle c1 = new Circle(5.75);
-            Console.WriteLine($"The perimeter for c1 is {c1.CalculatePerimeter():N3}, and the area is {c1.CalculateArea()}");
+            Console.WriteLine($"The perimeter for c1 is {c1.CalculatePerimeter():N3}, and the area is {c1.CalculateArea():n2}");
+
+            foreach (var rectangle in rectangles)
+            {
+                Console.WriteLine(rectangle.CalculateArea());
+               Console.WriteLine(rectangle.CalculatePerimeter());
+            }
 
             Console.ReadKey();
 
